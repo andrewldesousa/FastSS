44c5ccbc8220435d1a593d536fa0012285dcdb77
andrewldesousa <axd866@miami.edu>
2020-12-19 02:33:33 +0100

    reworked project structure

--- webapp/api/app.py
+++ webapp/api/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 from ask_sdk_core.skill_builder import SkillBuilder
 from flask_ask_sdk.skill_adapter import SkillAdapter, VERIFY_SIGNATURE_APP_CONFIG, VERIFY_TIMESTAMP_APP_CONFIG 
-from alexa.fast_synonym.lambda_function import LaunchRequestHandler, sb as fast_synonym_sb
+from alexa.fast_synonym import LaunchRequestHandler, sb as fast_synonym_sb
 
 sb = SkillBuilder()
 # Register all handlers, interceptors etc.
@@ -9,12 +9,16 @@ sb = SkillBuilder()
 
 sb.add_request_handler(LaunchRequestHandler())
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='pages/static', template_folder='pages')
 app.config.setdefault(VERIFY_SIGNATURE_APP_CONFIG, False) 
 app.config.setdefault(VERIFY_TIMESTAMP_APP_CONFIG, False) 
 
 skill_response = SkillAdapter(skill=fast_synonym_sb.create(), skill_id=None, app=app)
-skill_response.register(app=app, route="/")
+skill_response.register(app=app, route="/fast-synonym")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
