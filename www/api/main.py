be3a9e34adeb6e372a9f047806056270ade548eb
andrewldesousa <axd866@miami.edu>
2020-12-24 15:30:39 +0100

    Added synonyms endpoint

--- www/api/main.py
+++ www/api/main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import os
+import tempfile
 
 from typing import List
 
@@ -11,6 +13,26 @@ from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 app = FastAPI()
 
 
-@app.get("/")
-def predict():
-    return {'status': 'SUCCEED'}
+@app.get("/synonyms")
+def predict(word: str):
+    file_name = 'temp_file.txt'
+    results = str(os.system(f'echo "{word}" | /fastText/fasttext nn /result/fil9.bin >> {file_name}'))
+    f = open('temp_file.txt', 'r')
+    lines = f.readlines()
+    i = 0
+    output = []
+    for line in lines:
+        line = line.split(' ')
+        if i == 0:
+            line = line[2:]
+        elif i == len(lines)-1:
+            break
+        else:
+            pass
+        i += 1
+        output.append(line[0])
+    f.close()
+    os.remove(file_name)
+    print(output)
+
+    return {'status': output}
